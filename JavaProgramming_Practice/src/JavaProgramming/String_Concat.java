ae714191a1ee72174404af798ff92bc286cb0f1c
aeinmutwalli <aeinmutwalli@users.noreply.github.com>
2022-10-12 22:13:39 +0530

    demo

---
+++ JavaProgramming_Practice/src/JavaProgramming/String_Concat.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package JavaProgramming;
+
+/**
+ * @author QurratulAeinMohammad
+ *
+ */
+public class String_Concat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		String x= "Hello";
+		String y="World";
+		double c= 12.33;
+		double d= 10.33;
+		
+		System.out.println(a+b);
+		System.out.println(x+y);
+		System.out.println(c+d);
+		System.out.println(a+b+(x+y)+c+d);
+	}
+
+}
