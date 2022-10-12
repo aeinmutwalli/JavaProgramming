0275c2a91578530e04427d0f17ceff1b84ea182a
aeinmutwalli <aeinmutwalli@users.noreply.github.com>
2022-10-12 22:27:49 +0530

    This Is Java Project

---
+++ Java/src/MainProgram/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package MainProgram;
+
+/**
+ * @author QurratulAeinMohammad
+ *
+ */
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome");
+	}
+}
